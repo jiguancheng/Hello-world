bbe357592b813292b411c6f72077e0b966a5f99e
jiguancheng <129858204+jiguancheng@users.noreply.github.com>
2023-06-01 13:43:57 +0800

    Update test_for_files.py

--- test_for_files.py
+++ test_for_files.py
@@ -3,7 +3,13 @@ import streamlit as st
 st.markdown('A test for file operations.')
 a = open('main.py', 'r')
 st.code(a.read(), 'python', line_numbers=True)
-b = open('main2.py', 'w')
+try:
+    b = open('main2.py', 'a+')
+except:
+    b = open('main2.py', 'w')
 b.write('A test.')
 b.close()
 st.text('Success.')
+b = open('main2.py', 'r')
+st.text(b.read())
+b.close()
